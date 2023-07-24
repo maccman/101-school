f8f00d64a93409b2d0d638ac2ca5ed859f721514
Alex MacCaw <alex@alexmaccaw.com>
2023-07-24 13:08:42 -0600

    header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,7 +2,8 @@ import Link from 'next/link'
 
 import { cn } from '@/lib/utils'
 
-import { Button } from '../ui/button'
+import { UserNav } from './user-nav'
+import { buttonVariants } from '../ui/button'
 
 interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -10,7 +11,7 @@ export function Header({ className }: HeaderProps) {
   return (
     <header
       className={cn(
-        'border-b px-2 flex space-x-3 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl',
+        'border-b px-2 flex bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl items-center',
         className,
       )}
     >
@@ -18,18 +19,24 @@ export function Header({ className }: HeaderProps) {
         <span className="flex space-x-3 font-bold text-sm px-3 py-2">101.school</span>
       </Link>
 
-      <div className="flex space-x-5">
-        <Link href={'/courses'}>
-          <Button variant="ghost" size={'sm'} className="w-full justify-start" asChild>
-            <span>Courses</span>
-          </Button>
+      <div className="flex space-x-2 ml-6">
+        <Link href="/courses" className={cn(buttonVariants({ variant: 'ghost' }))}>
+          Courses
         </Link>
 
-        <Link href={'/about'}>
-          <Button variant="ghost" size={'sm'} className="w-full justify-start" asChild>
-            <span>About</span>
-          </Button>
+        <Link href="/about" className={cn(buttonVariants({ variant: 'ghost' }))}>
+          About
         </Link>
+      </div>
+
+      <div className="flex-1" />
+
+      <div className="flex space-x-5 items-center px-2">
+        <Link href="/courses/new" className={cn(buttonVariants({ variant: 'ghost' }))}>
+          Generate a course with AI...
+        </Link>
+
+        <UserNav />
       </div>
     </header>
   )

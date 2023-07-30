4dd10219b2689c9cb1eab5bbc4971252924e2322
Alex MacCaw <alex@alexmaccaw.com>
2023-07-30 13:30:47 -0400

    lint

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -124,7 +124,7 @@ export const reducer = (state: State, action: Action): State => {
   }
 }
 
-const listeners: Array<(state: State) => void> = []
+const listeners: Array<(_state: State) => void> = []
 
 let memoryState: State = { toasts: [] }
 

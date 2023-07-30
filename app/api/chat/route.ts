f8dc0018397dbb0ef7848900e2f7b51d0d864b06
Alex MacCaw <alex@alexmaccaw.com>
2023-07-30 16:11:55 -0400

    add chat

---
+++ app/api/chat/route.ts
@@ -0,0 +1,27 @@
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { Configuration, OpenAIApi } from 'openai-edge'
+
+import { withAuth } from '@/server/helpers/auth'
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(config)
+
+export const runtime = 'edge'
+
+async function createChat(req: Request, { userId }: { userId: string }) {
+  console.log('createChat', { userId })
+
+  const { messages } = await req.json()
+  const response = await openai.createChatCompletion({
+    model: 'gpt-4',
+    stream: true,
+    messages,
+  })
+
+  const stream = OpenAIStream(response)
+  return new StreamingTextResponse(stream)
+}
+
+export const POST = withAuth(createChat)

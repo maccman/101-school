ec0d5394740ce4dede9daf51d1357773f2f2134d
Alex MacCaw <alex@alexmaccaw.com>
2023-07-31 18:13:16 -0400

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,20 @@
 import { HeaderLayout } from '@/components/layouts/header-layout'
 
 export default function About() {
-  return <HeaderLayout>TODO</HeaderLayout>
+  return (
+    <HeaderLayout>
+      <div className="prose p-10 prose-headings:text-xl">
+        <h1>About</h1>
+        <p>101.school is an experiment in creating AI generated course contents.</p>
+
+        <p>
+          You enter a subject you&apos;re curious about and we generate a 13 week course.
+        </p>
+
+        <p>
+          An <a href="https://alexmaccaw.com">Alex MacCaw</a> project.
+        </p>
+      </div>
+    </HeaderLayout>
+  )
 }

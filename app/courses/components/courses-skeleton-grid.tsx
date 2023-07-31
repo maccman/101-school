8b2c5a4ce8708dc2ebbb56ea32ead9c634a8156a
Alex MacCaw <alex@alexmaccaw.com>
2023-07-31 08:49:40 -0400

    improve skeleton

--- app/courses/components/courses-skeleton-grid.tsx
+++ app/courses/components/courses-skeleton-grid.tsx
@@ -4,7 +4,10 @@ export async function CoursesSkeletonGrid() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
       {Array.from({ length: 8 }).map((_, index) => (
-        <Skeleton key={index} className="h-[227px]" />
+        <div key={index} className="space-y-2">
+          <Skeleton className="h-[30px] w-2/3" />
+          <Skeleton className="h-[227px]" />
+        </div>
       ))}
     </div>
   )

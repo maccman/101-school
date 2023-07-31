6f8b444262a661eaafffbe81e9cb41e2a6498892
Alex MacCaw <alex@alexmaccaw.com>
2023-07-31 08:47:25 -0400

    fix height

--- app/courses/components/courses-skeleton-grid.tsx
+++ app/courses/components/courses-skeleton-grid.tsx
@@ -4,7 +4,7 @@ export async function CoursesSkeletonGrid() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
       {Array.from({ length: 8 }).map((_, index) => (
-        <Skeleton key={index} />
+        <Skeleton key={index} className="h-[227px]" />
       ))}
     </div>
   )

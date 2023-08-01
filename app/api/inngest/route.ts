a956a82a9b74b5eb1e04450cbda0be69cc297c91
Alex MacCaw <alex@alexmaccaw.com>
2023-07-31 23:31:40 -0400

    node runtime

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -4,4 +4,6 @@ import { inngest } from '@/server/inngest/client'
 import { courseGenerate } from '@/server/inngest/functions/course-generate'
 import { helloWorld } from '@/server/inngest/functions/hello-world'
 
+export const runtime = 'node'
+
 export const { GET, POST, PUT } = serve(inngest, [helloWorld, courseGenerate])

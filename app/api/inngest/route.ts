20db65ec30ea27e826c26747c4e60a2f3768ea67
Alex MacCaw <alex@alexmaccaw.com>
2023-07-31 23:00:18 -0400

    serve function

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,10 +1,7 @@
 import { serve } from 'inngest/next'
 
 import { inngest } from '@/server/inngest/client'
+import { courseGenerate } from '@/server/inngest/functions/course-generate'
 import { helloWorld } from '@/server/inngest/functions/hello-world'
 
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve(inngest, [
-  /* your functions will be passed here later! */
-  helloWorld,
-])
+export const { GET, POST, PUT } = serve(inngest, [helloWorld, courseGenerate])

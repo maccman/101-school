5d17211cd9b97a381be4d30f500608dd86b5b42e
Alex MacCaw <alex@alexmaccaw.com>
2023-08-02 17:33:06 -0400

    lil refactor

--- server/emails/course-created-email.tsx
+++ server/emails/course-created-email.tsx
@@ -1,14 +1,16 @@
-import { Body } from '@react-email/body'
-import { Container } from '@react-email/container'
-import { Head } from '@react-email/head'
-import { Heading } from '@react-email/heading'
-import { Html } from '@react-email/html'
-import { Img } from '@react-email/img'
-import { Link } from '@react-email/link'
-import { Preview } from '@react-email/preview'
-import { Section } from '@react-email/section'
-import { Tailwind } from '@react-email/tailwind'
-import { Text } from '@react-email/text'
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+} from '@react-email/components'
 
 interface CourseCreatedEmailProps {
   userName: string | null
--- server/emails/course-unit-email.tsx
+++ server/emails/course-unit-email.tsx
@@ -1,13 +1,15 @@
-import { Body } from '@react-email/body'
-import { Container } from '@react-email/container'
-import { Head } from '@react-email/head'
-import { Heading } from '@react-email/heading'
-import { Html } from '@react-email/html'
-import { Img } from '@react-email/img'
-import { Link } from '@react-email/link'
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Img,
+  Link,
+  Section,
+  Text,
+} from '@react-email/components'
 import { Markdown } from '@react-email/markdown'
-import { Section } from '@react-email/section'
-import { Text } from '@react-email/text'
 
 interface CourseUnitEmailProps {
   course: {

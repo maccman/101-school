25e30dbe75774d4bd69c22c4d911319e15ca4352
Alex MacCaw <alex@alexmaccaw.com>
2023-08-03 12:32:41 -0400

    truncate

--- components/courses/course-card.tsx
+++ components/courses/course-card.tsx
@@ -14,7 +14,11 @@ export function CourseCard({ course, ...props }: Props) {
     <Link href={`/courses/${course.slug}`} {...props}>
       <div className="space-y-1">
         <h3 className="text-lg">{course.title}</h3>
-        {course.description && <h4 className="text-sm">{course.description}</h4>}
+        {course.description && (
+          <h4 className="text-sm truncate" title={course.description}>
+            {course.description}
+          </h4>
+        )}
         {course.image?.source && (
           <div className="rounded-md overflow-hidden max-w-[500px] min-h-[227px]">
             <img

a9206fc9bc22177177bc308a54800bf537c32df0
Alex MacCaw <alex@alexmaccaw.com>
2023-08-03 13:40:10 -0400

    lint

--- app/courses/[courseSlug]/components/course-generating.tsx
+++ app/courses/[courseSlug]/components/course-generating.tsx
@@ -1,3 +1,5 @@
+import { CheckCircle2 } from 'lucide-react'
+
 import { HeaderLayout } from '@/components/layouts/header-layout'
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
@@ -5,12 +7,17 @@ export function CourseGenerating() {
   return (
     <HeaderLayout>
       <div className="flex-1 flex items-center justify-center">
-        <Card className="text-center">
+        <Card className="text-center border-none shadow-none">
           <CardHeader className="space-y-5">
-            <CardTitle>Just one sec</CardTitle>
-            <CardDescription className="text-lg">
-              <p>Your course is generating...</p>
-              <p>You will receive an email once it&apos;s ready.</p>
+            <CheckCircle2 className="mx-auto h-12 w-12 text-primary" />
+
+            <CardTitle>Just one sec...</CardTitle>
+
+            <CardDescription className="text-lg max-w-[350px]">
+              <p>
+                Your course is generating. You&apos;ll will receive an email once
+                it&apos;s ready.
+              </p>
             </CardDescription>
           </CardHeader>
         </Card>

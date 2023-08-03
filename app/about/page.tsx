50a747cdf3fd83f3290a2a291cf6fdd276d7269a
Alex MacCaw <alex@alexmaccaw.com>
2023-08-03 10:31:57 -0400

    tweak

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,9 +8,13 @@ export default function About() {
         <p>101.school is an experiment in creating AI generated course contents.</p>
 
         <p>
-          It works like this: You enter something you&apos;re curious about, and then we
-          generate a 13 week course on the subject. You can choose to receive the course
-          via email, or read it on the site. We&apos;ll keep track of your progress.
+          It works like this: you enter something you&apos;re curious about, and then we
+          generate a 13 week course on the subject.
+        </p>
+
+        <p>
+          You can choose to receive the course via email, or read it on the site.
+          We&apos;ll keep track of your progress.
         </p>
 
         <p>

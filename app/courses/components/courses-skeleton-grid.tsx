6fd0ee6483b3f8094629941ca28ec0b3fb88f1a4
Alex MacCaw <alex@alexmaccaw.com>
2023-08-06 16:02:27 -0400

    tweak

--- app/courses/components/courses-skeleton-grid.tsx
+++ app/courses/components/courses-skeleton-grid.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 export async function CoursesSkeletonGrid() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
-      {Array.from({ length: 8 }).map((_, index) => (
+      {Array.from({ length: 12 }).map((_, index) => (
         <div key={index} className="space-y-2">
           <Skeleton className="h-[30px] w-2/3" />
           <Skeleton className="h-[227px]" />

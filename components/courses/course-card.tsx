be76f569f829301fa41ee95316c11cf1c1c4709b
Alex MacCaw <alex@alexmaccaw.com>
2023-08-15 13:04:34 -0400

    tweaks

--- components/courses/course-card.tsx
+++ components/courses/course-card.tsx
@@ -3,6 +3,8 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
 import Link from 'next/link'
 
+import { cn } from '@/lib/utils'
+
 import { Skeleton } from '../ui/skeleton'
 
 interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
@@ -15,32 +17,34 @@ interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   }
 }
 
-export function CourseCard({ course, ...props }: Props) {
+export function CourseCard({ course, className, ...props }: Props) {
   const headline = course.headline || course.description
 
   return (
-    <Link href={`/courses/${course.slug}`} {...props}>
-      <div className="space-y-2">
-        <div className="rounded-md overflow-hidden max-w-[500px]">
-          <Avatar>
-            <AvatarImage
-              src={course.image?.source}
-              className="h-full w-full min-h-[150px] object-cover aspect-video transition-all hover:scale-105 bg-muted"
-            />
-            <AvatarFallback>
-              <Skeleton className="min-h-[150px] w-full" />
-            </AvatarFallback>
-          </Avatar>
-        </div>
+    <Link
+      href={`/courses/${course.slug}`}
+      className={cn('flex flex-col space-y-2', className)}
+      {...props}
+    >
+      <div className="rounded-md overflow-hidden max-w-[500px] flex-1 flex flex-col">
+        <Avatar className="flex flex-1 flex-col">
+          <AvatarImage
+            src={course.image?.source}
+            className="h-full w-full min-h-[150px] object-cover aspect-video transition-all hover:scale-105 bg-muted"
+          />
+          <AvatarFallback className="flex-1 flex flex-col">
+            <Skeleton className="min-h-[150px] h-full w-full" />
+          </AvatarFallback>
+        </Avatar>
+      </div>
 
-        <div>
-          <h3 className="truncate font-medium tracking-tight">{course.title}</h3>
-          {headline && (
-            <h4 className="text-sm text-muted-foreground truncate" title={headline}>
-              {headline}
-            </h4>
-          )}
-        </div>
+      <div className="flex-none">
+        <h3 className="truncate font-medium tracking-tight">{course.title}</h3>
+        {headline && (
+          <h4 className="text-sm text-muted-foreground truncate" title={headline}>
+            {headline}
+          </h4>
+        )}
       </div>
     </Link>
   )

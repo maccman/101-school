953e8dff3fd4dd46b2afdb7b46fff2d99b6f542a
Alex MacCaw <alex@alexmaccaw.com>
2023-09-11 16:13:57 -0400

    redirect if generated

--- app/payments/success/page.tsx
+++ app/payments/success/page.tsx
@@ -1,5 +1,18 @@
-import { CourseGenerating } from '@/components/courses/course-generating'
+import { redirect } from 'next/navigation'
 
-export default function PaymentSuccess() {
+import { CourseGenerating } from '@/components/courses/course-generating'
+import { getCourse } from '@/server/db/courses/getters'
+
+export default async function PaymentSuccess({
+  searchParams,
+}: {
+  searchParams?: { courseId: string }
+}) {
+  const course = searchParams?.courseId ? await getCourse(searchParams.courseId) : null
+
+  if (course?.generatedAt) {
+    redirect(`/courses/${course.slug}`)
+  }
+
   return <CourseGenerating />
 }

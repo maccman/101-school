875cd958a29ebcb2aaa578c57816877fd88a3be3
Alex MacCaw <alex@alexmaccaw.com>
2024-04-18 18:38:35 -0400

    Fix error msg

--- app/courses/[courseSlug]/page.tsx
+++ app/courses/[courseSlug]/page.tsx
@@ -4,6 +4,7 @@ import { CourseUnit } from '@/components/course-units/course-unit'
 import { getCourseBySlugOrId, getFirstCourseUnit } from '@/server/db/courses/getters'
 import { getModule } from '@/server/db/modules/getters'
 import CourseSidebarLayout from '@/components/layouts/course-sidebar-layout'
+import { CourseGenerating } from '@/components/courses/course-generating'
 
 export default async function CoursePage({ params }: { params: { courseSlug: string } }) {
   const course = await getCourseBySlugOrId(params.courseSlug)
@@ -15,8 +16,10 @@ export default async function CoursePage({ params }: { params: { courseSlug: str
   const courseUnit = await getFirstCourseUnit(course.id)
 
   if (!courseUnit) {
-    console.error(`Course with slug "${params.courseSlug}" has no units`)
-    return notFound()
+    console.error(
+      `Course with slug "${params.courseSlug}" does not have a first module and unit`,
+    )
+    return <CourseGenerating />
   }
 
   const courseModule = await getModule(courseUnit.moduleId)

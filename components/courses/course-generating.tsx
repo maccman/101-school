c539de66de045204c3ebce0d6016d789ea741297
Alex MacCaw <alex@alexmaccaw.com>
2024-04-18 18:40:55 -0400

    double layout

--- components/courses/course-generating.tsx
+++ components/courses/course-generating.tsx
@@ -1,27 +1,24 @@
 import { CheckCircle2 } from 'lucide-react'
 
-import { HeaderLayout } from '@/components/layouts/header-layout'
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
 export function CourseGenerating() {
   return (
-    <HeaderLayout>
-      <div className="flex-1 flex items-center justify-center">
-        <Card className="text-center border-none shadow-none">
-          <CardHeader className="space-y-5">
-            <CheckCircle2 className="mx-auto h-12 w-12 text-primary" />
+    <div className="flex-1 flex items-center justify-center">
+      <Card className="text-center border-none shadow-none">
+        <CardHeader className="space-y-5">
+          <CheckCircle2 className="mx-auto h-12 w-12 text-primary" />
 
-            <CardTitle>Just one sec...</CardTitle>
+          <CardTitle>Just one sec...</CardTitle>
 
-            <CardDescription className="text-lg max-w-[350px]">
-              <p>
-                Your course is generating. You&apos;ll will receive an email once
-                it&apos;s ready.
-              </p>
-            </CardDescription>
-          </CardHeader>
-        </Card>
-      </div>
-    </HeaderLayout>
+          <CardDescription className="text-lg max-w-[350px]">
+            <p>
+              Your course is generating. You&apos;ll will receive an email once it&apos;s
+              ready.
+            </p>
+          </CardDescription>
+        </CardHeader>
+      </Card>
+    </div>
   )
 }
